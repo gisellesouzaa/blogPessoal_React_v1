7e634b816241eedd1387c99ce494d2602d60c64a
Giselle Souza <reallovetour@gmail.com>
2023-05-15 13:56:20 -0300

    Primeira versão Navbar, Home e Footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React from 'react';
+import GithubIcon from '@material-ui/icons/Github';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FolderSpecialIcon from '@material-ui/icons/FolderSpecial';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import { Typography, Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
 
-function Footer () {
+function Footer() {
     return (
         <>
-        <h1>footer</h1>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.linkedin.com/in/giselle-de-souza-gabriel/" target="_blank">
+                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/gisellesouzaa" target="_blank">
+                                <GithubIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://gisellesouzaa.github.io/portfolio/" target="_blank">
+                                <FolderSpecialIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://api.whatsapp.com/send?phone=5511999059579&text=Ol%C3%A1!%20Tudo%20bem%3F%20Vi%20seu%20portf%C3%B3lio" target="_blank">
+                                <WhatsAppIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Desenvolvedora: Giselle Souza</Typography>
+                            </a>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
         </>
     )
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React from 'react';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { Box } from '@mui/material';
 
-function Home () {
+function Home() {
     return (
         <>
-        <h1>home</h1>
-        
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://www.guiaviagensbrasil.com/imagens/Imagem%20do%20mar%20calma%20e%20belo%20da%20Praia%20da%20Engenhoca-Itacar%C3%A9-Bahia-BA.jpg" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+
         </>
     )
 }
