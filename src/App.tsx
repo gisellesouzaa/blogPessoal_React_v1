826fc0c662af0f54715e066ea88e151a88635518
Giselle Souza <reallovetour@gmail.com>
2023-05-16 10:30:38 -0300

    Implementado rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
 import Footer from './components/estaticos/footer/Footer';
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
