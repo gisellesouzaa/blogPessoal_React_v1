02fa7140fe9f56cc34019d54d9569dede1fa1b5e
Giselle Souza <reallovetour@gmail.com>
2023-05-18 14:28:32 -0300

    Comunicação com o Backend-Login-Componente

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,38 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { api } from '../../services/Service';
 import './Login.css';
+import UserLogin from '../../models/UserLogin';
 
 function Login() {
+
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+    )
+
+    function updateModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect(()=> {
+        if(token != ''){
+            history.push('/home')
+        }
+    }, [token])
+
+    async function onSubmit(e: ChangeEvent<HTMLInputElement>) {
+        e.preventDefault();
+        try {
+            const resposta = await api.post(`/usuarios/logar`, userLogin)
+            setToken(resposta.data.token)
+
+            alert('Usuário logado com sucesso!')
+        } catch(error){
+            alert('Dados do usuário inconsistentes, Erro ao logar!');
+        }
+
+        // console.log('userLogin: ' + Object.values(userLogin));
+    }
+
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                    <form onSubmit={onSubmit}>
+
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>
+                            Entrar
+                        </Typography>
+
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                            </Link>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
                         </Box>
                     </form>
+
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
+
                         <Link to="/cadastrousuario">
                             <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
                         </Link>
+
                     </Box>
                 </Box>
             </Grid>
-            <Grid className='imagem'>
-            </Grid>
+
+            <Grid className='imagem'></Grid>
 
         </Grid>
     )
