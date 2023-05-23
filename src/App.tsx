e9ecb90d28d178a866a615f239953bd1553b1650
Giselle Souza <reallovetour@gmail.com>
2023-05-23 17:45:46 -0300

    Modificado cores padrão do Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,35 @@ import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 import ListaTema from './components/temas/listatema/ListaTema';
-import './App.css'
 import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
 import CadastroTema from './components/temas/cadastrarTema/CadastroTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
-
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import './App.css'
 
 function App() {
+  const THEME = createTheme({
+    palette: {
+      primary: {
+        light: '#33c9dc',
+        main: '#00bcd4',
+        dark: '#008394',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#637bfe',
+        main: '#6625ff',
+        dark: '#2a3eb1',
+        contrastText: '#fff',
+      },
+    },
+  });
+
   return (
     <>
+      <MuiThemeProvider theme={THEME}>
+
         <BrowserRouter>
           <Navbar />
           <Routes>
@@ -37,13 +56,14 @@ function App() {
             <Route path="/formularioPostagem" element={<CadastroPost />} />
             <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
             <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
-            
+
             <Route path="/formularioTema" element={<CadastroTema />} />
             <Route path="/formularioTema/:id" element={<CadastroTema />} />
             <Route path="/deletarTema/:id" element={<DeletarTema />} />
           </Routes>
           <Footer />
         </BrowserRouter>
+      </MuiThemeProvider>
     </>
   )
 }
