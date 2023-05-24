0de548463f00f6f174d3b4bf8a55970de970ca43
Giselle Souza <reallovetour@gmail.com>
2023-05-24 20:46:57 -0300

    Nova página de login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -30,8 +30,8 @@ function Login() {
         })
     }
 
-    useEffect(()=> {
-        if(token != ''){
+    useEffect(() => {
+        if (token != '') {
             history('/home')
         }
     }, [token])
@@ -40,9 +40,8 @@ function Login() {
         e.preventDefault();
         try {
             await login(`/usuarios/logar`, userLogin, setToken)
-
             alert('Usuário logado com sucesso!')
-        } catch(error){
+        } catch (error) {
             alert('Dados do usuário inconsistentes, Erro ao logar!');
         }
 
@@ -52,8 +51,8 @@ function Login() {
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
-                <Box paddingX={20}>
-                    <form onSubmit={onSubmit}>
+                <Box paddingX={20} >
+                    {/* <form onSubmit={onSubmit}>
 
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>
                             Entrar
@@ -68,8 +67,52 @@ function Login() {
                                 Logar
                             </Button>
                         </Box>
-                    </form>
+                    </form> */}
 
+                    {/* <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                        </Box>
+
+                        <Link to="/cadastro">
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
+
+                    </Box> */}
+                </Box>
+            </Grid>
+
+            <Grid alignItems='center' xs={6} className='containerVideo'>
+                <video autoPlay muted loop id="myVideo">
+                    <source src="src\video\ocean-sea-wave-video.mp4" type="video/mp4" />
+                </video>
+                <Box className="content">
+
+                        <Box mx={1} textAlign='center'>
+                            <img className="logo_principal" src="src\images\logo_vetor.png" alt="" />
+                        </Box>
+                    <form onSubmit={onSubmit}>
+
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='tituloEntrar'>
+                            Entrar
+                        </Typography>
+
+                        <TextField
+                            value={userLogin.usuario}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
+                            id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+
+                        <TextField
+                            value={userLogin.senha}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
+                            id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+
+                        <Box marginTop={1} textAlign='center'>
+                            <Button type='submit' variant='contained' color='primary' fullWidth>
+                                Logar
+                            </Button>
+                        </Box>
+                    </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
@@ -82,8 +125,6 @@ function Login() {
                     </Box>
                 </Box>
             </Grid>
-
-            <Grid className='imagem'></Grid>
 
         </Grid>
     )
