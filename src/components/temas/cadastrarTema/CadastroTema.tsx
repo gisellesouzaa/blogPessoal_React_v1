0342a2fa800941a0a461aedf188999ce6d86e6df
Giselle Souza <reallovetour@gmail.com>
2023-05-29 14:16:07 -0300

    Fix token

--- src/components/temas/cadastrarTema/CadastroTema.tsx
+++ src/components/temas/cadastrarTema/CadastroTema.tsx
@@ -5,11 +5,16 @@ import { buscaId, post, put } from '../../../services/Service';
 import { useNavigate, useParams } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
 import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function CadastroTema() {
     let navigate = useNavigate();
     const { id } = useParams<{ id: string }>();
-    const [token, setToken] = useLocalStorage('token');
+    // const [token, setToken] = useLocalStorage('token');
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
     const [tema, setTema] = useState<Tema>({
         id: 0,
         descricao: ''
